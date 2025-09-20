a02b416b25dabbf9ba5162b5f6dbfc7a56acb4c8
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-09-20 11:52:21 +0300

    Initial commit

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else if (/^\d+$/.test(dateString)) { 
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
